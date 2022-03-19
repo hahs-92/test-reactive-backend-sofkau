92641aa7dbb5727107b1eb0d942bf2d4656e8720
Alex <alexandre_2420@outlook.com>
2022-03-18 21:10:30 -0500

    add uppercaseConverse Class and its Test

---
+++ src/test/java/com/sofkau/springwebfluxtest/UppercaseConverterTest.java
@@ -0,0 +1,21 @@
+package com.sofkau.springwebfluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
+
+@SpringBootTest
+public class UppercaseConverterTest {
+    final TestPublisher<String> testPublisher = TestPublisher.create();
+
+    @Test
+    void testUpperCase() {
+        UppercaseConverter uppercaseConverter = new UppercaseConverter(testPublisher.flux());
+
+        StepVerifier.create(uppercaseConverter.getUpperCase())
+                .then(() -> testPublisher.emit("datos", "GeNeRaDoS", "Sofka"))
+                .expectNext("DATOS", "GENERADOS", "SOFKA")
+                .verifyComplete();
+    }
+}
